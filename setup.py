8fe5656e5855270b16c3a421c7699b3b5c3b0330
Zooko O'Whielacronx <zooko@zooko.com>
2007-12-12 17:10:55 -0800

    pycryptopp: setup: pass --quiet to invocation of darcsver in setup.py

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ trove_classifiers=[
 
 try:
     import os
-    (cin, cout, cerr,)= os.popen3("darcsver")
+    (cin, cout, cerr,)= os.popen3("darcsver --quiet")
     print cout.read()
 except Exception, le:
     pass
