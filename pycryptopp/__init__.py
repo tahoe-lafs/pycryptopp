f3ab3f0cf267af100ca986d90f8e6518fbe8ed66
Zooko O'Whielacronx <zooko@zooko.com>
2007-12-21 20:58:37 -0800

    pycryptopp: update doc in the __init__.py file concerning automatically generated version stamps

--- pycryptopp/__init__.py
+++ pycryptopp/__init__.py
@@ -6,8 +6,9 @@ __version__ = "unknown"
 try:
     from _version import __version__
 except ImportError:
-    # we're running in a tree that hasn't run darcsver, so we don't
-    # know what our version is. This should not usually happen.
+    # We're running in a tree that hasn't run darcsver from the pyutil library,
+    # and didn't come with a _version.py, so we don't know what our version
+    # is. This should not happen very often.
     pass
 
 import publickey, hash, cipher
