92de0836355d21d02fd65c2de8e6e88dbf1beda0
Zooko O'Whielacronx <zooko@zooko.com>
2008-01-30 14:02:46 -0800

    tests: for test_sha256, add test vectors including Monte test from the NIST SHS programme

--- pycryptopp/test/test_sha256.py
+++ pycryptopp/test/test_sha256.py
@@ -4,7 +4,7 @@ import cStringIO, os, random, re
 
 import unittest
 
-from binascii import b2a_hex
+from binascii import b2a_hex, a2b_hex
 
 global VERBOSE
 VERBOSE=False
@@ -25,33 +25,37 @@ def ab(x): # debuggery
 def randstr(n):
     return ''.join(map(chr, map(random.randrange, [0]*n, [256]*n)))
 
-h0 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-h_bd = "68325720aabd7c82f30f554b313d0570c95accbb7dc4b5aae11204c08ffe732b"
-h_5fd4 = "7c4fbf484498d21b487b9d61de8914b2eadaf2698712936d47c3ada2558f6788"
+h0 = a2b_hex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+h_bd = a2b_hex("68325720aabd7c82f30f554b313d0570c95accbb7dc4b5aae11204c08ffe732b")
+h_5fd4 = a2b_hex("7c4fbf484498d21b487b9d61de8914b2eadaf2698712936d47c3ada2558f6788")
 
 class SHA256(unittest.TestCase):
     def test_digest(self):
         empty_digest = sha256.SHA256().digest()
         self.failUnless(isinstance(empty_digest, str))
-        self.failUnless(len(empty_digest) == 32)
-        self.failUnless(b2a_hex(empty_digest) == h0)
-        #empty_hexdigest = sha256.SHA256().hexdigest()
+        self.failUnlessEqual(len(empty_digest), 32)
+        self.failUnlessEqual(empty_digest, h0)
+
+    def test_hexdigest(self):
+        empty_hexdigest = sha256.SHA256().hexdigest()
+        self.failUnlessEqual(a2b_hex(empty_hexdigest), h0)
+    test_hexdigest.todo = "Not yet implemented: SHA256.hexdigest()."
 
     def test_onebyte_1(self):
         d = sha256.SHA256("\xbd").digest()
-        self.failUnless(b2a_hex(d) == h_bd)
+        self.failUnlessEqual(d, h_bd)
 
     def test_onebyte_2(self):
         s = sha256.SHA256()
         s.update("\xbd")
         d = s.digest()
-        self.failUnless(b2a_hex(d) == h_bd)
+        self.failUnlessEqual(d, h_bd)
 
     def test_update(self):
         s = sha256.SHA256("\x5f")
         s.update("\xd4")
         d = s.digest()
-        self.failUnless(b2a_hex(d) == h_5fd4)
+        self.failUnlessEqual(d, h_5fd4)
 
     def test_constructor_type_check(self):
         self.failUnlessRaises(TypeError, sha256.SHA256, None)
@@ -65,7 +69,7 @@ class SHA256(unittest.TestCase):
         d1 = h.digest()
         self.failUnless(isinstance(d1, str))
         d2 = h.digest()
-        self.failUnless(d1 == d2)
+        self.failUnlessEqual(d1, d2)
 
     def test_digest_then_update_fail(self):
         h = sha256.SHA256()
@@ -74,6 +78,56 @@ class SHA256(unittest.TestCase):
             h.update("oops")
         except sha256.Error, le:
             self.failUnless("digest() has been called" in str(le), le)
+
+VECTS_RE=re.compile("\nLen = ([0-9]+)\nMsg = ([0-9a-f]+)\nMD = ([0-9a-f]+)")
+
+class SHSVectors(unittest.TestCase):
+    """
+    All of the SHA-256 test vectors from the NIST SHS, in the files distributed
+    by NIST.  (NIST distributes them in a .zip, but we expect them to be
+    unpacked and in a subdirectory named 'vectors').
+    """
+    def test_short(self):
+        return self._test_vect_file(open(os.path.join('pycryptopp', 'test', 'vectors', 'SHA256ShortMsg.txt'), 'rU'))
+
+    def test_long(self):
+        return self._test_vect_file(open(os.path.join('pycryptopp', 'test', 'vectors', 'SHA256LongMsg.txt'), 'rU'))
+
+    def _test_vect_file(self, infile):
+        vects_str = infile.read()
+        for mo in VECTS_RE.finditer(vects_str):
+            msglenbits = int(mo.group(1))
+            assert msglenbits % 8 == 0
+            msglen = msglenbits / 8
+            msg = a2b_hex(mo.group(2))[:msglen] # The slice is necessary because NIST seems to think that "00" is a reasonable representation for the zero-length string.
+            assert len(msg) == msglen, (len(msg), msglen)
+            md = a2b_hex(mo.group(3))
+
+            computed_md = sha256.SHA256(msg).digest()
+            self.failUnlessEqual(computed_md, md)
+
+    def test_monte(self):
+        infile = open(os.path.join('pycryptopp', 'test', 'vectors', 'SHA256Monte.txt'), 'rU')
+        for line in infile:
+            line = line.strip()
+            if line[:7] == 'Seed = ':
+                seed = a2b_hex(line[7:])
+                break
+
+        j = 0
+        for line in infile:
+            line = line.strip()
+            if line[:8] == 'COUNT = ':
+                assert int(line[8:]) == j
+            elif line[:5] == 'MD = ':
+                mds = []
+                mds.append(seed);mds.append(seed);mds.append(seed);
+                for i in range(1000):
+                    m = mds[-3]+mds[-2]+mds[-1]
+                    mds.append(sha256.SHA256(m).digest())
+                seed = mds[-1]
+                self.failUnlessEqual(line[5:], b2a_hex(seed))
+                j += 1
 
 if __name__ == "__main__":
     unittest.main()
