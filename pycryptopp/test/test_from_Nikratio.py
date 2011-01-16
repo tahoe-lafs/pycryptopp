6fa8a0d97ed0b1af551bf378a9795acd00e45aec
Zooko O'Whielacronx <zooko@zooko.com>
2011-01-16 00:02:21 -0800

    tests: add test from Nikratio's report in ticket #67

---
+++ pycryptopp/test/test_from_Nikratio.py
@@ -0,0 +1,28 @@
+import unittest
+
+# This was reported as triggering a "Use of uninitialised value of
+# size 4" under valgrind by Nikratio in pycryptopp-0.5.17 and Crypto++
+# 5.6.0. See http://tahoe-lafs.org/trac/pycryptopp/ticket/67
+
+class T(unittest.TestCase):
+    def test_t(self):
+        import hmac
+        import pycryptopp
+        import hashlib
+        import struct
+
+        def encrypt(buf, passphrase, nonce):
+
+            key = hashlib.sha256(passphrase + nonce).digest()
+            cipher = pycryptopp.cipher.aes.AES(key)
+            hmac_ = hmac.new(key, digestmod=hashlib.sha256)
+
+            hmac_.update(buf)
+            buf = cipher.process(buf)
+            hash_ = cipher.process(hmac_.digest())
+
+            return b''.join(
+                            (struct.pack(b'<B', len(nonce)),
+                            nonce, hash_, buf))
+
+        encrypt('foobar', 'passphrase', 'nonce')
