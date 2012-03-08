15e33b4aa626667cf5718668027d2d5b17b9afe8
Zooko O'Whielacronx <zooko@zooko.com>
2012-03-07 17:11:17 -0700

    port the "zookoapi" branch from python-ed25519 (functionality) This is derived from [5413869880cbde227d2cc9831ed818ac9ad0f701], [597a5e091b94190ee346b29561de404f8139631f], and [0f5a2536ca4d7494e93d46e266ff4509a6237338]. Also I used the string method .encode('hex') instead of the function hexlify, but otherwise I didn't intentionally make any other changes and this is intended to be a straight port of the code.

--- src/pycryptopp/publickey/ed25519/__init__.py
+++ src/pycryptopp/publickey/ed25519/__init__.py
@@ -1,10 +1,6 @@
-from keys import (BadSignatureError, BadPrefixError,
-                  create_keypair, SigningKey, VerifyingKey,
-                  remove_prefix, to_ascii, from_ascii)
+from keys import (BadSignatureError, SigningKey, VerifyingKey)
 
-(BadSignatureError, BadPrefixError,
- create_keypair, SigningKey, VerifyingKey,
- remove_prefix, to_ascii, from_ascii) # hush pyflakes
+(BadSignatureError, SigningKey, VerifyingKey) # hush pyflakes
 
 from _version import get_versions
 __version__ = get_versions()['version']
