99e8e7a1f483fd958f4e194f59f4fc9fe825c59e
Zooko O'Whielacronx <zooko@zooko.com>
2012-07-12 10:29:28 -0300

    benchmark the fast elliptic curve sigs many times for more reliable measurement
    But don't benchmark the RSA key-generation many times, as it takes way too long
    for that.

--- src/pycryptopp/bench/bench_sigs.py
+++ src/pycryptopp/bench/bench_sigs.py
@@ -1,6 +1,6 @@
 from pycryptopp.publickey import ecdsa, ed25519, rsa
 
-from common import insecurerandstr, rep_bench, print_bench_footer
+from common import insecurerandstr, rep_bench
 
 msg = 'crypto libraries should come with benchmarks'
 
@@ -25,6 +25,34 @@ class ECDSA256(object):
         signer = ecdsa.SigningKey(self.seed)
         self.sig = signer.sign(msg)
         self.verifier = signer.get_verifying_key()
+        
+    def ver(self, N):
+        sig = self.sig
+        verifier = self.verifier
+        for i in xrange(N):
+            verifier.verify(sig, msg)
+        
+class Ed25519(object):
+    def __init__(self):
+        self.seed = insecurerandstr(32)
+        self.signer = None
+
+    def gen(self, N):
+        for i in xrange(N):
+             ed25519.SigningKey(self.seed)
+        
+    def sign_init(self, N):
+        self.signer = ed25519.SigningKey(self.seed)
+        
+    def sign(self, N):
+        signer = self.signer
+        for i in xrange(N):
+            signer.sign(msg)
+        
+    def ver_init(self, N):
+        signer = ed25519.SigningKey(self.seed)
+        self.sig = signer.sign(msg)
+        self.verifier = ed25519.VerifyingKey(signer.get_verifying_key_bytes())
         
     def ver(self, N):
         sig = self.sig
@@ -119,19 +147,29 @@ class RSA3248(object):
             verifier.verify(msg, sig)
         
 def bench_sigs(MAXTIME):
-    for klass in [ECDSA256, Ed25519, RSA2048, RSA3248]:
+    for klass in [ECDSA256, Ed25519,]:
         print klass
         ob = klass()
         print "generate key"
-        rep_bench(ob.gen, 4, UNITS_PER_SECOND=1000, MAXTIME=MAXTIME, MAXREPS=100)
+        rep_bench(ob.gen, 1000, UNITS_PER_SECOND=1000, MAXTIME=MAXTIME, MAXREPS=100)
         print "sign"
-        rep_bench(ob.sign, 20, UNITS_PER_SECOND=1000, initfunc=ob.sign_init, MAXTIME=MAXTIME, MAXREPS=100)
+        rep_bench(ob.sign, 1000, UNITS_PER_SECOND=1000, initfunc=ob.sign_init, MAXTIME=MAXTIME, MAXREPS=100)
         print "verify"
-        rep_bench(ob.ver, 100, UNITS_PER_SECOND=1000, initfunc=ob.ver_init, MAXTIME=MAXTIME, MAXREPS=100)
+        rep_bench(ob.ver, 1000, UNITS_PER_SECOND=1000, initfunc=ob.ver_init, MAXTIME=MAXTIME, MAXREPS=100)
+        print
+
+    for klass in [RSA2048, RSA3248]:
+        print klass
+        ob = klass()
+        print "generate key"
+        rep_bench(ob.gen, 1, UNITS_PER_SECOND=1000, MAXTIME=MAXTIME, MAXREPS=100)
+        print "sign"
+        rep_bench(ob.sign, 1000, UNITS_PER_SECOND=1000, initfunc=ob.sign_init, MAXTIME=MAXTIME, MAXREPS=100)
+        print "verify"
+        rep_bench(ob.ver, 10000, UNITS_PER_SECOND=1000, initfunc=ob.ver_init, MAXTIME=MAXTIME, MAXREPS=100)
         print
 
     print "milliseconds per operation"
-    print_bench_footer(UNITS_PER_SECOND=1000)
     print
 
 def bench(MAXTIME=10.0):
