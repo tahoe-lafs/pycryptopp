47bb9e56e360bfc7f3d67ec9ac57243b9b823463
Zooko O'Whielacronx <zooko@zooko.com>
2012-07-12 10:33:44 -0300

    improve accuracy of the fallback-benchmarker (when no pyutil)
    By using time.clock on windows instead of time.time
    
    Also remove shebang lines.

--- src/pycryptopp/bench/common.py
+++ src/pycryptopp/bench/common.py
@@ -4,52 +4,64 @@ try:
     import pyutil.benchutil
     rep_bench = pyutil.benchutil.rep_bench
 except (ImportError, AttributeError):
+    import platform, time
+    if 'windows' in platform.system().lower():
+        clock = time.clock
+    else:
+        clock = time.time
+
     def this_rep_bench(func, N, UNITS_PER_SECOND, MAXTIME, MAXREPS, initfunc=None):
-        import time
+        tt = time.time
 
         if initfunc is not None:
             initfunc(N)
 
-        mean = 0
+        meanc = 0
         MAXREPS = 100
 
-        timeout = time.time() + MAXTIME
+        timeout = tt() + MAXTIME
 
         for i in range(MAXREPS):
-            start = time.time()
+            startc = clock()
 
             func(N)
 
-            stop = time.time()
+            stopc = clock()
 
-            mean += (stop - start)
+            deltac = stopc - startc
+            if deltac <= 0:
+                print "clock jump backward or wrapped -- ignoring this sample. startc: %s, stopc: %s, deltac: %s" % (startc, stopc, deltac,)
+            else:
+                meanc += deltac
 
-            if stop >= timeout:
+            if time.time() >= timeout:
                 break
 
         num = i+1
-        mean *= UNITS_PER_SECOND
-        mean /= num
-        mean /= N
+        meanc *= UNITS_PER_SECOND
+        meanc /= num
+        meanc /= N
 
         res = {
-            'mean': mean/num,
+            'meanc': meanc,
             'num': num
             }
-        print "mean: %(mean)#8.03e (of %(num)6d)" % res
+        print "mean: %(meanc)#8.03e (of %(num)6d)" % res
     rep_bench = this_rep_bench
-
-try:
-    import pyutil.benchutil
-    print_bench_footer = pyutil.benchutil.print_bench_footer
-except (ImportError, AttributeError):
-    def this_print_bench_footer(UNITS_PER_SECOND=1):
-        from decimal import Decimal as D
-
-        print "time units per second: %s; seconds per time unit: %s" % (UNITS_PER_SECOND, D(1)/UNITS_PER_SECOND)
-    print_bench_footer = this_print_bench_footer
 
 import random as insecurerandom
 def insecurerandstr(n):
     return ''.join(map(chr, map(insecurerandom.randrange, [0]*n, [256]*n)))
 
+def calib_clock():
+    interval = 1.0
+    
+    import time
+    tc = time.clock
+    tt = time.time
+
+    def measure_sleep(x, clock):
+        st = clock()
+        time.sleep(x)
+        
+    
--- src/pycryptopp/test/test_aes.py
+++ src/pycryptopp/test/test_aes.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import random, re
 
 import unittest
