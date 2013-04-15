f16a712b11dd1516e419c8a6148048bfcf3ba8b7
Zooko O'Whielacronx <zooko@zooko.com>
2013-04-14 18:10:34 -0600

    print out the version of pycryptopp in bench

--- src/pycryptopp/bench/bench_algs.py
+++ src/pycryptopp/bench/bench_algs.py
@@ -1,6 +1,12 @@
 import bench_sigs, bench_ciphers, bench_hashes
 
 def bench(MAXTIME=10.0):
+    import pycryptopp
+    print pycryptopp
+    print pycryptopp.__version__,
+    print pycryptopp._pycryptopp.cryptopp_version
+    import pkg_resources
+    print pkg_resources.require('pycryptopp')
     bench_sigs.bench(MAXTIME)
     bench_ciphers.bench(MAXTIME)
     bench_hashes.bench(MAXTIME)
