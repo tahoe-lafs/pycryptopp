5e61502522ab86d890450b5ab7a840db13cfead2
Zooko O'Whielacronx <zooko@zooko.com>
2013-04-19 15:41:57 -0600

    I found yet another version number hidden in here.

--- src/pycryptopp/bench/bench_algs.py
+++ src/pycryptopp/bench/bench_algs.py
@@ -1,12 +1,16 @@
 import bench_sigs, bench_ciphers, bench_hashes
 
-def bench(MAXTIME=10.0):
+def print_versions():
     import pycryptopp
-    print pycryptopp
-    print pycryptopp.__version__,
-    print pycryptopp._pycryptopp.cryptopp_version
+    print "pycryptopp: ", pycryptopp
+    print "pycryptopp.__version__: ", pycryptopp.__version__,
+    print "pycryptopp._pycryptopp.cryptopp_version: ", pycryptopp._pycryptopp.cryptopp_version
+    print "pycryptopp.publickey.ed25519._version.get_versions(): ", pycryptopp.publickey.ed25519._version.get_versions()
     import pkg_resources
-    print pkg_resources.require('pycryptopp')
+    print "pkg_resources.require('pycryptopp'): ", pkg_resources.require('pycryptopp')
+
+def bench(MAXTIME=10.0):
+    print_versions()
     bench_sigs.bench(MAXTIME)
     bench_ciphers.bench(MAXTIME)
     bench_hashes.bench(MAXTIME)
