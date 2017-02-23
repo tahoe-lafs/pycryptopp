9df7762108c4c3e5966f441a95ddbac448ba7bae
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-02-23 15:20:58 -0500

    Maybe auditwheel does do something, I don't know.

--- misc/build_helpers/_build-wheels.sh
+++ misc/build_helpers/_build-wheels.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Copied straight from https://github.com/pypa/python-manylinux-demo
+# Based on https://github.com/pypa/python-manylinux-demo
 
 set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/cp2*/bin; do
-    "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
+    "${PYBIN}/pip" wheel /io/ -w /wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels and write them to their
+# final location.
+for whl in /wheelhouse/pycrypto-*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
 done
